4e6d753c7569a3d5e8ccdc519868e422471d9cf7
Modurotolu Olokode <durolawk@gmail.com>
2020-11-12 19:13:18 +0100

    handled form state

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,40 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const initialLoginField = {
+  name: "",
+  email: "",
+  password: "",
+  repassword: "",
+}
+
 function App() {
+  const [loginField, setLoginField] = useState(initialLoginField);
+  
+  const onValueChange = e => {
+    setLoginField({
+      ...loginField,
+      [e.target.name]: e.target.value
+    });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <form>
           <h1>Create account</h1>
           <label htmlFor="name">Your name
-            <input type="text" value="" name="name" />
+            <input type="text" value={loginField.name} onChange={onValueChange} name="name" />
           </label>
           <label htmlFor="email">Email
-            <input type="text" value="" name="email" />
+            <input type="text" value={loginField.email} onChange={onValueChange} name="email" />
           </label>
           <label htmlFor="password">Password
-            <input type="password" value="" name="password" />
+            <input type="password" value={loginField.password} onChange={onValueChange} name="password" />
           </label>
           <label htmlFor="repassword">Re-enter password
-            <input type="password" value="" name="repassword" />
+            <input type="password" value={loginField.repassword} onChange={onValueChange} name="repassword" />
           </label>
           <button>Create your Amazon account</button>
         </form>
