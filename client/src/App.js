042350cb6d97ac7a06e2282ecb4a7c6f2e069a20
Modurotolu Olokode <durolawk@gmail.com>
2020-11-14 05:54:13 +0100

    resized the form

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
+import logo from './images/logo.jpg';
 import './App.css';
 
 const initialLoginField = {
@@ -21,6 +21,7 @@ function App() {
 
   return (
     <div className="App">
+      <img className="logo" src={logo} />
       <form>
         <h1>Create account</h1>
         <label htmlFor="name">Your name
@@ -31,11 +32,14 @@ function App() {
         </label>
         <label htmlFor="password">Password
             <input type="password" value={loginField.password} onChange={onValueChange} name="password" />
+        <p className="password-text">Passwords must be at least 6 characters.</p>
         </label>
         <label htmlFor="repassword">Re-enter password
             <input type="password" value={loginField.repassword} onChange={onValueChange} name="repassword" />
         </label>
         <button>Create your Amazon account</button>
+        <p>By creating an account, you agree to Amazon's <a>Conditions of Use</a> and <a>Privacy Notice</a>.</p>
+        <p>Already have an account? Sign-In</p>
       </form>
     </div>
   );
